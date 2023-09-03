abf699fbbe2149c987ed895fa389faa5b822730a
Gleb <desgleb@gmail.com>
2023-09-03 15:16:25 +0300

    tsconfig.json done

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -1,1 +1,24 @@
+const a: number = 45;
+let b: string = '45';
+
+class Person {
+    // noinspection JSUnusedLocalSymbols
+    constructor(private name: string) {}
+}
+
+const me = new Person('Gleb');
+
 console.log('App is running');
+console.log(a, b);
+
+console.log(me);
+
+// demo comment
+
+const map = new Map();
+
+const btn = document.querySelector('#btn')!;
+
+btn.addEventListener('click', () => {
+    console.log('Btn clicked!');
+});
