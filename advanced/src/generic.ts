186b9ef4becf97e8ad329c36bdd117d62dae3b0f
Gleb <desgleb@gmail.com>
2023-09-04 08:13:11 +0300

    generics done

--- advanced/src/generic.ts
+++ advanced/src/generic.ts
@@ -87,3 +87,30 @@ class Collection<T extends string | number | boolean> {
 
 //=============================
 
+interface Car {
+    model: string;
+    year: number;
+}
+
+function createAndValidateCar(model: string, year: number): Car {
+    const car: Partial<Car> = {};
+
+    if (model.length > 3) car.model = model;
+
+    if (year > 2000) car.year = year;
+
+    return car as Car;
+}
+
+// ============================
+
+const cars: Readonly<Array<string>> = ['Ford', 'Audi'];
+// cars.shift();
+// cars[1];
+
+const ford: Readonly<Car> = {
+    model: 'Ford',
+    year: 2020
+}
+
+// ford.model = 'Ferrari';
