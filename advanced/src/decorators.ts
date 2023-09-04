6650e2991244fe5fa3ff33e65f42faa1c2806d33
Gleb <desgleb@gmail.com>
2023-09-04 15:48:06 +0300

    decorators done

--- advanced/src/decorators.ts
+++ advanced/src/decorators.ts
@@ -85,8 +85,23 @@ const validators: ValidatorConfig = {}
 
 function Required(target: any, propName: string) {
     validators[target.constructor.name] = {
+        ...validators[target.constructor.name],
         [propName]: 'required'
     }
+}
+
+function validate(obj: any): boolean {
+    const objConfig = validators[obj.constructor.name];
+    if (!objConfig) {
+        return true;
+    }
+    let isValid = true;
+    Object.keys(objConfig).forEach(key => {
+       if (objConfig[key] === 'required') {
+        isValid = isValid && !!obj[key];
+       }
+    });
+    return isValid;
 }
 
 class Form {
@@ -97,3 +112,12 @@ class Form {
         this.email = email;
     }
 }
+
+const form = new Form('d@mail.com');
+
+if (validate(form)) {
+    console.log('Valid: ', form);
+} else {
+    console.log('Validation Error');
+}
+
